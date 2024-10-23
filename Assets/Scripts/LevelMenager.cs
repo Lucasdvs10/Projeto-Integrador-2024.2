8dd1b9ccb1f109ef9bc32cac544845d024e20959
Eduardo Takase Sawada <sawadaeduardo@gmail.com>
2024-10-23 03:43:26 -0300

    Level Selector Created
    fizemos algumas alterações no script de level manager e criamos o level selector com as cenas para as futuras gravações

--- Assets/Scripts/LevelMenager.cs
+++ Assets/Scripts/LevelMenager.cs
@@ -3,21 +3,23 @@ using UnityEngine.SceneManagement;
 
 namespace DefaultNamespace {
     public class LevelMenager : MonoBehaviour {
-        [SerializeField] private GameObject _nextLevelBtn;
+        // [SerializeField] private GameObject _nextLevelBtn;
+        //reutilizei o seu codigo para o level selector qqr coisa me avisa q eu crio outro script
 
         private void Awake() {
             int nextLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
             int lastLevelIndex = SceneManager.sceneCountInBuildSettings - 1;
 
             if (nextLevelIndex > lastLevelIndex) {
-                _nextLevelBtn.SetActive(false);
+                // _nextLevelBtn.SetActive(false);
                 Debug.Log("Não há mais níveis disponíveis!", this);
             }
         }
 
-        public void LoadNextLevel() {
-            int nextLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        public void LoadNextLevel(int index) {
+            int nextLevelIndex = SceneManager.GetActiveScene().buildIndex + index;
             SceneManager.LoadScene(nextLevelIndex);
+            //os index das scenes precisam ser configuradas no build configs
         }
     }
 }
